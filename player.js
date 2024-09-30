05d77956480eeccbab98c53b59d1cf2d4967bb20
WasathTheekshana <wasath.vt@gmail.com>
2024-09-30 21:48:12 +0530

    feat: Gravity physics to the player object

--- player.js
+++ player.js
@@ -2,18 +2,34 @@ class Player {
     constructor(game) {
         this.game = game;
         this.x = 0;
-        this.y = 0;
+        this.y;
         this.spriteWidth = 200;
         this.spriteHeight = 200;
-        this.width  = 100;
-        this.height  = 100;
+        this.width;
+        this.height;
+        this.speedY;
     }
     draw() {
         this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    update () {}
+    update () {
+        this.y += this.speedY;
+        if (!this.isTouchingBottom()) {
+            this.speedY += this.game.gravity;
+        }
+
+        // Botoom boundary
+        if (this.isTouchingBottom()) {
+            this.y = this.game.height - this.height;
+        }
+    }
     resize() {
         this.width = this.spriteWidth * this.game.ratio;
         this.height = this.spriteHeight * this.game.ratio;
+        this.y = this.game.height * 0.5 - this.height * 0.5;
+        this.speedY = -4 * this.game.ratio;
+    }
+    isTouchingBottom() {
+        return this.y >= this.game.height - this.height;
     }
 }
